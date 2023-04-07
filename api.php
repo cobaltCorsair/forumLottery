824518392238cf0a45b612a69552545df2129b6f
Leraje <45194081+cobaltCorsair@users.noreply.github.com>
2023-04-07 11:31:35 +0300

    Update api.php

--- api.php
+++ api.php
@@ -4,6 +4,11 @@
 	header("Access-Control-Allow-Methods: POST");
 	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 	
+	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {	
+		http_response_code(200);
+		exit();
+	}
+	
 	$secretKey = "MybbLottery";
 	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
